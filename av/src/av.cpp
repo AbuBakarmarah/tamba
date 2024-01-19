95559a82f6556f5333cdab4303edeca1985209db
Abu Marah <abubakarrmarah45@gamil.com>
2024-01-18 21:29:21 -0500

    adding c++ code

---
+++ av/src/av.cpp
@@ -0,0 +1,33 @@
+//============================================================================
+// Name        : av.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int addOne(int &);
+
+int main () {
+
+	int n = 3;
+
+	cout << "n before calling addOne(): " << n << endl;
+	cout << "value returned by addOne(): " << addOne(n) << endl;
+	cout << "n after calling addOne() is: " << n << endl;
+
+}
+int addOne( int &value){
+	value++;
+
+
+
+    return value;
+}
+
+
+
+
